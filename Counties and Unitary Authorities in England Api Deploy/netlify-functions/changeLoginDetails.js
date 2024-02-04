0a344d1d90f075a3a8bf1b575706affd1059a741
Okechukwu Enetanya <129814429+Enetanya@users.noreply.github.com>
2024-02-04 22:09:56 +0000

    Update changeLoginDetails.js

--- Counties and Unitary Authorities in England Api Deploy/netlify-functions/changeLoginDetails.js
+++ Counties and Unitary Authorities in England Api Deploy/netlify-functions/changeLoginDetails.js
@@ -25,7 +25,7 @@ router.post('/submit-email', async (req, res) => {
     ); 
 }
  const token = generateToken(email);
-  const changeLink = `http://localhost:3000/forgot/change-login-details/${token}`;
+  const changeLink = `https://charming-figolla-3e81b7.netlify.app/forgot/change-login-details/${token}`;
  
  // Create a nodemailer transporter 
  const transporter = 
