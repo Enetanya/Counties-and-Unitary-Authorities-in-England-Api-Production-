d76ff341853483fc9c0cdeff1bc0f09ac1da107f
Okechukwu Enetanya <129814429+Enetanya@users.noreply.github.com>
2024-02-15 11:01:11 +0000

    Update changeLoginDetails.js

--- Counties and Unitary Authorities in England Api Deploy/netlify-functions/changeLoginDetails.js
+++ Counties and Unitary Authorities in England Api Deploy/netlify-functions/changeLoginDetails.js
@@ -121,6 +121,9 @@ router.get('/sse', (req, res) => {
   res.setHeader('Connection', 'keep-alive');
   res.setHeader('Access-Control-Allow-Origin', '*');
 
+  // Set timeout on the response object
+  res.setTimeout(0); // Timeout disabled, set to 0
+
   // Connect the SSE instance to the response stream
   sse.init(req, res);
 
