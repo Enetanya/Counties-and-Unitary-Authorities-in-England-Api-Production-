6adbeea9071f261cbdce245dbc64019dfe814a29
Okechukwu Enetanya <129814429+Enetanya@users.noreply.github.com>
2024-02-27 14:34:47 +0000

    Update model2.js

--- Counties and Unitary Authorities in England Api Deploy/model2.js
+++ Counties and Unitary Authorities in England Api Deploy/model2.js
@@ -1,1 +1,14 @@
+const mongoose = require('./connection.js');
 
+
+// Define a mongoose schema for storing the random number
+const RandomNumberSchema = new mongoose.Schema({
+  number: {
+    type: Number,
+    required: true,
+  },
+});
+
+const RandomNumber = mongoose.model('RandomNumber', RandomNumberSchema,'resolutionNumber');
+
+module.exports = RandomNumber;
